147f03302d207676f909b5b95e2f7924a9488dcf
atomnoid <aayushs2580@gmail.com>
2025-01-13 15:44:27 +0530

    did some changes and created logic building folder

--- Hackerrank problems/find_the_percentage.py
+++ Hackerrank problems/find_the_percentage.py
@@ -1,12 +1,12 @@
 #Hackerrank problem "Find the percentage"
 if __name__ == '__main__':
     n = int(input())
-student_marks = {}
-for _ in range(n):
-    name, *line = input().split()
-    scores = list(map(float, line))
-    student_marks[name] = scores
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
     query_name = input()
 if query_name in student_marks.keys():
-    avg = float(sum(student_marks[query_name]) / len(student_marks[query_name]))
+    avg= float(sum(student_marks[query_name]) / len(student_marks[query_name]))
     print(f"{avg:.2f}")
