5b6c05541473789c27b75afc2d10664eb51b75f5
atomnoid <aayushs2580@gmail.com>
2025-01-18 00:41:09 +0530

    solved a very basic lottery logic problem

---
+++ Logic building in python/lottery.py
@@ -0,0 +1,11 @@
+print("hey wellcome to atomnoid lottery")
+x = int(input("enter your ticket number: "))
+
+if (x==345) or (x==457) or (x==578):
+    print("You won a car")
+elif(x==348) or (x==897) or (x==467):
+    print("you won a bike")
+elif(x==999) or (x==100) or (x==890):
+    print("you won an iphone")
+else:
+    print("better luck next time")
