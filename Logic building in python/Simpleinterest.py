8528bd86f9ff027cc35e9631c9ef598a39b98a92
atomnoid <aayushs2580@gmail.com>
2025-01-19 21:33:30 +0530

    solved a basic SI code

---
+++ Logic building in python/Simpleinterest.py
@@ -0,0 +1,7 @@
+#Simple interest calculator
+p =  float(input("Enter the principal amount: "))
+r = float(input("Enter the rate of interest: "))
+t = float(input("Enter the time period: "))
+
+si = (p*r*t)/100
+print("The simple interest is: ", si)
