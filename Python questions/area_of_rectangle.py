dd7869a0bb310a85e583a90d2f97a4beb83adcce
atomnoid <aayushs2580@gmail.com>
2025-01-25 00:58:59 +0530

    solved a basic find a area of rectangle question

---
+++ Python questions/area_of_rectangle.py
@@ -0,0 +1,5 @@
+#write a program to find the area of a rectangle.
+length = int(input("Enter the length of the rectangle: "))
+breadth = int(input("Enter the breadth of the rectangle: "))
+area = length * breadth
+print ("The area of a rectangle is: ", area)
