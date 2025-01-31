06b24959a07b128a9cdd174e5d8745e86173cfe5
atomnoid <aayushs2580@gmail.com>
2025-02-01 00:57:21 +0530

    solved a basic lower to upper case problem

---
+++ Python questions/lowertoupper.py
@@ -0,0 +1,6 @@
+# A python program to convert lower case to upper case
+ch = input("Enter the lower case character: ")
+if ch == ch.lower():
+    print(ch.upper())
+else:
+    print("given character is not lower case")
