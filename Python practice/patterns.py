6bccfd1315a9de88f2aaa84128f39dcc37878b5a
atomnoid <aayushs2580@gmail.com>
2025-06-13 02:11:29 +0530

    solved some square patterns using for loops

---
+++ Python practice/patterns.py
@@ -0,0 +1,22 @@
+#Learning to print patterns 
+
+#Square patterns  
+n = int(input("enter a num")) 
+for j in range (1, n+1): #outer loop which will iterate the code for every line
+    for i in range(1, n+1):#inner loop which will print the n in the same line 
+        if i != n:
+            print(i, end=' ')
+        else:
+            print(i)
+
+#Same question with pattern
+n = int(input("Enter the size of the square: "))
+
+for i in range(n):
+    for j in range(n):
+        print("*", end=" ")
+    print()
+
+
+n2 = int(input("enter your number:"))
+
