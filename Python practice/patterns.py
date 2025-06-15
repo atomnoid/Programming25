26924df019a25ad708f2583dae6354cc68c0f948
atomnoid <aayushs2580@gmail.com>
2025-06-16 00:23:16 +0530

    solved a reversed number triangle pattern

--- Python practice/patterns.py
+++ Python practice/patterns.py
@@ -100,3 +100,11 @@ for i in range (1, size+1):
         print(j, end=" ")
     print()
 
+#Reverse number triangle
+
+size = int(input("enter the size of the triangle"))
+n = 1
+for i in range (1, size+1):
+    for j in range(i, 0, -1):
+        print(j, end=" ")
+    print()
