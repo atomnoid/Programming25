db087c9c6137923ce82e934c35f68cb7ea3cf83b
atomnoid <aayushs2580@gmail.com>
2025-06-17 23:00:14 +0530

    used test.py to run code and removed the previous codes

--- Python project/test.py
+++ Python project/test.py
@@ -1,20 +1,5 @@
 #Here you will not see any code cause i just run my code here for testing
 
 
-size = int(input("enter your size:"))
 
-for i in range (1, size+1):
-    for j in range(i - 1):
-        print(" ", end=' ')
-    for k in range(size - i + 1):
-        print(i, end=' ')
-    print()
 
-size = int(input("Enter the number of rows: "))
-
-for i in range(1, size + 1):
-    for j in range(i - 1):
-        print(" ", end=" ")
-    for k in range(size - i + 1):
-        print(i, end=" ")
-    print()
