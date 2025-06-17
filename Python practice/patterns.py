1939d4140ac3d6948adfee561c34c97af20f1ee7
atomnoid <aayushs2580@gmail.com>
2025-06-17 18:26:01 +0530

    solved a left shifted inverted triangle problem

--- Python practice/patterns.py
+++ Python practice/patterns.py
@@ -117,3 +117,14 @@ for i in range(1, size+1):
     for j in range(size - i + 1):
         print(i, end='')
     print()
+
+#Left shifted inverted triangle
+
+size = int(input("enter your size:"))
+
+for i in range (1, size+1):
+    for j in range(i - 1):
+        print(" ", end=' ')
+    for k in range(size - i + 1):
+        print(i, end=' ')
+    print()
--- Python project/test.py
+++ Python project/test.py
@@ -1,9 +1,20 @@
 #Here you will not see any code cause i just run my code here for testing
 
 
-size = int(input("enter the number"))
+size = int(input("enter your size:"))
 
-for i in range(1, size+1):
-    for j in range(size - i + 1):
-        print(i, end='')
+for i in range (1, size+1):
+    for j in range(i - 1):
+        print(" ", end=' ')
+    for k in range(size - i + 1):
+        print(i, end=' ')
+    print()
+
+size = int(input("Enter the number of rows: "))
+
+for i in range(1, size + 1):
+    for j in range(i - 1):
+        print(" ", end=" ")
+    for k in range(size - i + 1):
+        print(i, end=" ")
     print()
