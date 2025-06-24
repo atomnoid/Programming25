2f79539b358c8c63553c6d84c5a0565cd8a52f8b
atomnoid <aayushs2580@gmail.com>
2025-06-24 13:33:22 +0530

    solved another question like two sum

--- DSA/arrays.py
+++ DSA/arrays.py
@@ -18,6 +18,20 @@
 
 
 
+# def twosum(arr, target):
+#     n = len(arr)
+#     for i in range(n):
+#         for j in range(i+1, n):
+#             if arr[i] + arr[j] == target:
+#                 return[i, j]
+#     return[]
+# arr = [1,3,4,7,8]
+# target = 7
+# print(twosum(arr, target))
+
+
+#question 3
+
 def twosum(arr, target):
     n = len(arr)
     for i in range(n):
@@ -25,6 +39,6 @@ def twosum(arr, target):
             if arr[i] + arr[j] == target:
                 return[i, j]
     return[]
-arr = [1,3,4,7,8]
-target = 7
+arr = [2, 4, 7, 9, 10]
+target = 19
 print(twosum(arr, target))
