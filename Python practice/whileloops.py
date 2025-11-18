92815c9ef9d64068b451b2e29d558ee6128fc312
atomnoid <aayushs2580@gmail.com>
2025-11-18 12:56:54 +0530

    pushed old changes

--- Python practice/whileloops.py
+++
@@ -1,8 +0,0 @@
-#while loop execute the code infinityy times
-
-name = input("enter your name: ")
-
-while name == "":
-    print("you did not type anything")
-    name = input("enter your name: ")
-print(f"hello {name}")
